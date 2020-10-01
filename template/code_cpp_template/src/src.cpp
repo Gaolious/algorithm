b2af7bfe4bf95d8af19178499d055994e1934108
ajava <gaolious@gmail.com>
2020-10-02 01:18:57 +0900

    [template] add ps template

---
+++ template/code_cpp_template/src/src.cpp
@@ -0,0 +1,158 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+using namespace std;
+
+namespace FIO 
+{
+    const int FD_STDIN = 0;
+    const int FD_STDOUT = 1;
+    const int FD_STDERR = 2;
+
+    const static int BUFF_SIZE = 1<<21;
+    static char buf[BUFF_SIZE];
+    static char *p = buf + BUFF_SIZE;
+    static int last_read = 0 ;
+    inline static bool readByte(char &ret)
+    {
+        if ( p - buf >= BUFF_SIZE )
+        {
+            last_read = (int)read(FD_STDIN, (void *)buf, sizeof(buf));
+            p = buf ;
+        }
+        else if ( last_read < BUFF_SIZE )
+        {
+            if ( (int)( p - buf) >= last_read ) // EOF
+                return false;
+        }
+        ret = *p++;
+        return true ;
+    }
+
+    inline bool readInt(int *ret)
+    {
+        int n = 0 ;
+        int flag = 0 ;
+        register char c ;
+        if (!readByte(c)) return false;
+        
+        while (c <= ' ') 
+            if ( !readByte(c) ) return false;
+        
+        if (c == '-') 
+        {
+            flag = 1;
+            if ( !readByte(c) ) return false;
+        }
+
+        while ( c > ' ' ) {
+            n = n * 10 + c - '0';
+            if (!readByte(c) ) break;
+        }
+        *ret = flag ? -n : n;
+        return true ;
+    }
+
+    inline bool readChar(FILE *fp, char *ret)
+    {
+        register char c;
+        if ( !readByte(c) ) return false ;
+        
+        while (c <= ' ') 
+            if (!readByte(c) ) return false ;
+
+        *ret = c;
+        return true ;
+    }
+
+    inline static int readLine(char *str, int maxLen )
+    {
+        register char c = 0x00 ;
+        int i ;
+
+        while (c < ' ' )
+            if ( !readByte(c) ) return false ;
+
+        for ( i = 0 ; i < maxLen && c && c != '\n' ; i ++ )
+        {
+            str[i] = c ;
+            if ( !readByte(c) ) 
+                break;
+        }
+        str[ i ] = 0x00 ;
+        return i;
+    }
+
+    char outbuf[BUFF_SIZE + 20];
+    char* outp = outbuf;
+
+    void printInt(int n)
+    {
+        unsigned nn;
+        if (n < 0) nn = -n, *outp++ = '-';
+        else nn = n;
+        char *end = outp;
+        do
+        {
+            *end++ = nn % 10 + '0';
+            nn /= 10;
+        } while (nn);
+        reverse(outp, end);
+        if (end < outbuf + BUFF_SIZE) outp = end;
+        else
+        {
+            syscall(0x01, 1, outbuf, end - outbuf);
+            outp = outbuf;
+        }
+    }
+
+    void printChar(char c)
+    {
+        char *end = outp;
+        *end++ = c;
+        if (end < outbuf + BUFF_SIZE) outp = end;
+        else
+        {
+            syscall(0x01, 1, outbuf, end - outbuf);
+            outp = outbuf;
+        }
+    }
+    void printNewline()
+    {
+        printChar('\n');
+    }
+    void flush()
+    {
+        syscall(0x01, 1, outbuf, outp - outbuf);
+    }    
+} 
+
+#define MAX_N (100)
+void process()
+{
+    
+}
+
+int main() 
+{ 
+#ifndef ONLINE_JUDGE 
+    clock_t t = clock();
+#else  
+#endif 
+
+    process(); 
+
+#ifndef ONLINE_JUDGE 
+    t = clock() - t;
+    printf ("Estimated Time : %f seconds.\n",((float)t)/CLOCKS_PER_SEC);    
+    printf("\n[Press Enter to Continue ...]");
+    fflush(stdout);
+    fflush(stdin);
+#else 
+#endif 
+    return 0; 
+} 
+
+
---
+++ template/ps_init.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+HOME_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+WORK_PATH=`pwd -P`
+
+CPP_TEMPLATE_PATH="$( cd "${HOME_PATH}"; cd ./code_cpp_template; pwd -P )"
+PYTHON_TEMPLATE_PATH="$( cd "${HOME_PATH}"; cd ./code_python_template; pwd -P )"
+
+#SITE=$1
+TYPE=$1
+PARAM=$2
+
+README_TITLE="BAEKJOON - {$PARAM}"
+PROBLEM_URL="https://www.acmicpc.net/problem/${PARAM}"
+
+function usage()
+{
+    echo "
+Usages :         
+    $ $0 (c|cpp|python2|python3) [problem no]
+"
+    exit
+}
+
+function gen_python2_readme()
+{
+    pushd "${WORK_PATH}/${PARAM}" > /dev/null 2>&1 || exit
+    echo " 
+## ${README_TITLE}
+
+### Problem        
+    url :  ${PROBLEM_URL}
+
+### IDE 
+\`\`\`bash
+$ code --version
+`code --version`
+\`\`\`
+
+### Compile
+\`\`\`bash
+$ python3 --version
+`python3 --version`
+
+$ make
+\`\`\`
+
+### run
+\`\`\`bash
+$ python 3 ./src/src.py < ./input/input.txt > ./output/output.txt
+\`\`\`
+
+" > ./readme.md
+
+    popd > /dev/null 2>&1
+}
+
+
+function gen_python3_readme()
+{
+    pushd "${WORK_PATH}/${PARAM}" > /dev/null 2>&1 || exit
+    echo " 
+## ${README_TITLE}
+
+### Problem        
+    url :  ${PROBLEM_URL}
+
+### IDE 
+\`\`\`bash
+$ code --version
+`code --version`
+\`\`\`
+
+### Compile
+\`\`\`bash
+$ python3 --version
+`python3 --version`
+
+$ make
+\`\`\`
+
+### run
+\`\`\`bash
+$ python 3 ./src/src.py < ./input/input.txt > ./output/output.txt
+\`\`\`
+
+" > ./readme.md
+
+    popd > /dev/null 2>&1
+}
+
+
+function gen_gcc_readme()
+{
+    pushd "${WORK_PATH}/${PARAM}" > /dev/null 2>&1 || exit
+    echo " 
+## ${README_TITLE}
+
+### Problem        
+    ${PROBLEM_URL}
+
+### IDE 
+\`\`\`bash
+$ code --version
+`code --version`
+\`\`\`
+
+### Compile
+\`\`\`bash
+$ gcc --version
+`gcc --version`
+
+$ make
+\`\`\`
+
+### run
+\`\`\`bash
+$ ./bin/UVA < ./input/input.txt > ./output/output.txt
+\`\`\`
+
+" > ./readme.md
+
+    popd > /dev/null 2>&1
+}
+
+function check_param()
+{
+    if ! [[ "${PARAM}" =~ ^[0-9]+$ ]]; then
+        echo "parameter $1 is not digit."
+        usage
+    fi
+}
+
+if [[ "${WORK_PATH}" == *"Baekjoon"* ]]; then
+    README_TITLE="BAEJOON - ${PARAM}"
+    PROBLEM_URL="url : https://www.acmicpc.net/problem/${PARAM}"
+    check_param
+elif [[ "${WORK_PATH}" == *"UVA"* ]]; then
+    README_TITLE="UVA - ${PARAM}"
+    PROBLEM_URL="url : https://onlinejudge.org/external/1/${PARAM}.pdf"
+    check_param
+else
+        README_TITLE="${PARAM}"
+        PROBLEM_URL=""
+fi
+
+if [[ -d "${WORK_PATH}/${PARAM}" ]]; then 
+    echo "path ${WORK_PATH}/${PARAM} is exist."; 
+    usage
+fi
+
+
+
+case ${TYPE} in
+    [Cc])
+        echo "Copy Templates..."
+        cp -rf "${CPP_TEMPLATE_PATH}" "${WORK_PATH}/${PARAM}"
+
+        echo "Generate readme.md"
+        gen_gcc_readme
+
+        echo "code"
+        /usr/bin/code "${WORK_PATH}/${PARAM}" "${WORK_PATH}/${PARAM}/src/src.cpp"
+        ;;
+
+    [Cc][Pp][Pp])
+        echo "Copy Templates..."
+        cp -rf "${CPP_TEMPLATE_PATH}" "${WORK_PATH}/${PARAM}"
+
+        echo "Generate readme.md"
+        gen_gcc_readme
+
+        echo "code"
+        /usr/bin/code "${WORK_PATH}/${PARAM}" "${WORK_PATH}/${PARAM}/src/src.cpp"
+        ;;
+
+    [Pp][Yy][Tt][Hh][Oo][Nn]2)
+        echo "Copy Templates..."
+        cp -rf "${PYTHON_TEMPLATE_PATH}" "${WORK_PATH}/${PARAM}"
+
+        echo "Generate readme.md"
+        gen_python3_readme
+
+        echo "code"
+        /usr/bin/code "${WORK_PATH}/${PARAM}" "${WORK_PATH}/${PARAM}/src/src.py"
+
+        ;;
+
+    [Pp][Yy][Tt][Hh][Oo][Nn]3)
+        echo "Copy Templates..."
+        cp -rf "${PYTHON_TEMPLATE_PATH}" "${WORK_PATH}/${PARAM}"
+
+        echo "Generate readme.md"
+        gen_python3_readme
+
+        echo "code"
+        /usr/bin/code "${WORK_PATH}/${PARAM}" "${WORK_PATH}/${PARAM}/src/src.py"
+
+
+        ;;
+    *)
+        echo "unknown type : ${TYPE}"
+        exit
+        ;;
+esac
+
