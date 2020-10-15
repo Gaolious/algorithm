1fc72c9a5d7ac71ed721f0c9b7e51dcc9fd26209
aJava <gaolious@gmail.com>
2020-10-16 04:55:16 +0900

    [UVA] 12279 - Emoogle Balance

---
+++ UVA-Online/Volume_122/12279/src/src.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+#include <sys/stat.h>
+
+using namespace std;
+class FIO
+{
+    char *p;
+public :
+    FIO()
+    {
+        struct stat rstat;
+        this->p = NULL ;
+        
+        if ( fstat(0, &rstat) == 0 && rstat.st_size > 0 )
+            this->p = (char*)mmap(0, rstat.st_size, PROT_READ, MAP_SHARED, 0, 0) ;
+        
+        if ( this->p == MAP_FAILED ) this->p = NULL ;
+    }
+    void skip() { while ( this->p && *this->p && *this->p <= ' ' ) this->p++; }
+    bool Char(char &c)  { if ( !this->p || *this->p <= 0 ) return false ; c = *this->p++; return true ; }
+    bool line(char *s, int &len, const int maxLen)
+    {
+        char c = 0 ;
+        this->skip();
+        
+        for ( len = 0 ; this->Char(c) && c > 0 && c != '\n' && len < maxLen ; len++ )
+            *s++ = c ;
+        *s = 0x00;
+        return c || len > 0;
+    }
+    template<typename T> bool Int(T &n)
+    {
+        char c ;
+        bool flag = true ;
+        n = 0 ;
+        this->skip();
+        if ( ! this->Char(c) )  return false ;
+        if ( c == '-' )
+        {
+            flag = false;
+            if ( ! this->Char(c) ) return false ;
+        }
+
+        while ( c > ' ' )
+        {
+            n = n * 10 + c - 48 ;
+            if ( ! this->Char(c) ) break;
+        }
+        if ( !flag ) n = -n;
+        return true ;
+    }
+    template<typename T> bool Int(T &a, T &b) { return this->Int(a) && this->Int(b); }
+    template<typename T> bool Int(T &a, T &b, T &c) { return this->Int(a, b) && this->Int(c); }
+    template<typename T> bool Int(T &a, T &b, T &c, T &d) { return this->Int(a, b) && this->Int(c, d); }
+};
+int max(int a, int b, int c) { return max(a, max(b,c)); }
+int min(int a, int b, int c) { return min(a, min(b,c)); }
+
+
+void process()
+{
+    FIO fio ;
+    int T, n, ret , i;
+
+    for ( i = 1 ; fio.Int(T) && T ; i ++ )
+    {
+        for ( ret=0 ; T-- && fio.Int(n)  ; ret += n == 0 ? -1 : 1);
+        printf("Case %d: %d\n", i, ret);
+    }
+
+}
+
+int main() 
+{ 
+#ifndef ONLINE_JUDGE 
+    clock_t t = clock();
+#else  
+#endif 
+
+    process(); 
+
+#ifndef ONLINE_JUDGE 
+    t = clock() - t;
+    printf ("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+    printf ("Estimated Time : %f seconds.\n",((float)t)/CLOCKS_PER_SEC);    
+    printf("\n[Press Enter to Continue ...]");
+    fflush(stdout);
+    fflush(stdin);
+#else 
+#endif 
+    return 0; 
+} 
+
+
