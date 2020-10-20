ce089e42db149147b4d496af1b6f474b7f42fe3d
aJava <gaolious@gmail.com>
2020-10-21 02:00:58 +0900

    [UVA] 12372 - Packing for Holiday

---
+++ UVA-Online/Volume_123/12372/src/src.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+#include <sys/stat.h>
+using namespace std;
+
+class FIO
+{
+    char *p;
+public :
+    FIO()
+    {
+        struct stat rstat;
+        this->p = NULL ;
+        
+        if ( fstat(0, &rstat) == 0 && rstat.st_size > 0 )
+            this->p = (char*)mmap(0, rstat.st_size, PROT_READ, MAP_SHARED, 0, 0) ;
+        
+        if ( this->p == MAP_FAILED ) this->p = NULL ;
+    }
+    void skip() { while ( this->p && *this->p && *this->p <= ' ' ) this->p++; }
+    bool Char(char &c)  { if ( !this->p || *this->p <= 0 ) return false ; c = *this->p++; return true ; }
+    bool line(char *s, int &len, const int maxLen)
+    {
+        char c = 0 ;
+        this->skip();
+        
+        for ( len = 0 ; this->Char(c) && c > 0 && c != '\n' && len < maxLen ; len++ )
+            *s++ = c ;
+        *s = 0x00;
+        return c || len > 0;
+    }
+    template<typename T> bool Int(T &n)
+    {
+        char c ;
+        bool flag = true ;
+        n = 0 ;
+        this->skip();
+        if ( ! this->Char(c) )  return false ;
+        if ( c == '-' )
+        {
+            flag = false;
+            if ( ! this->Char(c) ) return false ;
+        }
+
+        while ( c > ' ' )
+        {
+            n = n * 10 + c - 48 ;
+            if ( ! this->Char(c) ) break;
+        }
+        if ( !flag ) n = -n;
+        return true ;
+    }
+    template<typename T> bool Int(T &a, T &b) { return this->Int(a) && this->Int(b); }
+    template<typename T> bool Int(T &a, T &b, T &c) { return this->Int(a, b) && this->Int(c); }
+    template<typename T> bool Int(T &a, T &b, T &c, T &d) { return this->Int(a, b) && this->Int(c, d); }
+};
+
+void process()
+{
+    FIO fin ;    
+    int N, x,y,z;
+    int i ;
+    fin.Int(N);
+    for ( i = 1 ; i <= N ; i ++ )
+    {
+        fin.Int(x,y,z);
+        printf("Case %d: %s\n", i, ( x <= 20 && y <= 20 && z <= 20 ) ? "good" : "bad" );
+    }
+}
+
+int main() 
+{ 
+#ifndef ONLINE_JUDGE 
+    clock_t t = clock();
+#else  
+#endif 
+
+    process(); 
+
+#ifndef ONLINE_JUDGE 
+    t = clock() - t;
+    printf ("Estimated Time : %f seconds.\n",((float)t)/CLOCKS_PER_SEC);    
+#else 
+#endif 
+    return 0; 
+} 
+
+
